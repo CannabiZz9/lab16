645a6330d1c88e1b229a438ff76791de30127f6b
CannabiZz9 <hydrahuracan1@gmail.com>
2023-02-07 15:04:34 +0700

    finish

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,21 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &w,int &x,int &y,int &z){
+	int a[4]={w,x,y,z};
+	int b[4] = {0,0,0,0};
+	for(int i=0;i<4;i++){
+		b[i] = rand()%4;
+		for(int j = 0;j<i;j++){
+			if(b[i] == b[j]){
+				i--;
+				break;
+			}
+		}
+	}
+	w = a[b[0]];
+	x = a[b[1]];
+	y = a[b[2]];
+	z = a[b[3]];
+}
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,27 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *w,int *x,int *y,int *z){
+	int a[4]={*w,*x,*y,*z};
+	int b[4] = {0,0,0,0};
+	for(int i=0;i<4;i++){
+		b[i] = rand()%4;
+		for(int j = 0;j<i;j++){
+			if(b[i] == b[j]){
+				i--;
+				break;
+			}
+		}
+	}
+	*w = a[b[0]];
+	*x = a[b[1]];
+	*y = a[b[2]];
+	*z = a[b[3]];
+}
